69ad22fffd7d34b55f3e515ad6c49b0cc7c945a7
Villar <johnvillar41@yahoo.com>
2021-03-30 20:52:39 +0800

    Moved Pages to Views Folder and Added Administrator Model

---
+++ Models/AdministratorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftEngWebEmployee.Models
+{
+    public class AdministratorModel
+    {
+        public int User_ID { get; set; }
+        public string User_Username { get; set; }
+        public string User_Password { get; set; }
+        public string Username { get; set; }
+        public string User_Image { get; set; }
+    }
+}
