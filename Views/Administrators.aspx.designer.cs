c026c12b951f95b041a6bed0f5b02d367cb7ad6e
Villar <johnvillar41@yahoo.com>
2021-04-09 14:26:59 +0800

    Added choice for adding user whether admin or employee

--- Views/Administrators.aspx.designer.cs
+++ Views/Administrators.aspx.designer.cs
@@ -132,6 +132,15 @@ namespace SoftEngWebEmployee.Views
         protected global::System.Web.UI.WebControls.FileUpload ImageUpload;
 
         /// <summary>
+        /// RadioButtonPosition control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonPosition;
+
+        /// <summary>
         /// BtnSave control.
         /// </summary>
         /// <remarks>
