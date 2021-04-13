d4eb28ba1ad994036c672a07bde07fc6dfcd264f
CHRISTIAN BERNABE <chenbernabe.712@gmail.com>
2021-04-13 16:03:58 +0800

    Added functions in ProductRepository - FetchGHProducts - FetchHPProducts

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -61,6 +61,62 @@ namespace SoftEngWebEmployee.Repository
         //View Product Details
         //Delete Product
 
+        public async Task<List<ProductModel>> FetchGHProducts()
+        {
+            List<ProductModel> productList = new List<ProductModel>();
+            using (MySqlConnection connection = new MySqlConnection(DbConnString.DBCONN_STRING))
+            {
+                await connection.OpenAsync();
+                string queryString = "SELECT * FROM products_table WHERE product_category='Greenhouse'";
+                MySqlCommand command = new MySqlCommand(queryString, connection);
+                MySqlDataReader reader = (MySqlDataReader)await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    productList.Add(
+                            new ProductModel()
+                            {
+                                Product_ID = int.Parse(reader["product_id"].ToString()),
+                                ProductName = reader["product_name"].ToString(),
+                                ProductDescription = reader["product_description"].ToString(),
+                                ProductPicture = reader["product_picture"].ToString(),
+                                ProductStocks = int.Parse(reader["product_stocks"].ToString()),
+                                ProductCategory = reader["product_category"].ToString(),
+                                ProductPrice = int.Parse(reader["product_price"].ToString())
+                            }
+                        );
+                }
+            }
+            return productList;
+        }
+
+        public async Task<List<ProductModel>> FetchHPProducts()
+        {
+            List<ProductModel> productList = new List<ProductModel>();
+            using (MySqlConnection connection = new MySqlConnection(DbConnString.DBCONN_STRING))
+            {
+                await connection.OpenAsync();
+                string queryString = "SELECT * FROM products_table WHERE product_category='Hydroponics'";
+                MySqlCommand command = new MySqlCommand(queryString, connection);
+                MySqlDataReader reader = (MySqlDataReader)await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    productList.Add(
+                            new ProductModel()
+                            {
+                                Product_ID = int.Parse(reader["product_id"].ToString()),
+                                ProductName = reader["product_name"].ToString(),
+                                ProductDescription = reader["product_description"].ToString(),
+                                ProductPicture = reader["product_picture"].ToString(),
+                                ProductStocks = int.Parse(reader["product_stocks"].ToString()),
+                                ProductCategory = reader["product_category"].ToString(),
+                                ProductPrice = int.Parse(reader["product_price"].ToString())
+                            }
+                        );
+                }
+            }
+            return productList;
+        }
+
         public async Task<ProductModel> GetProducts(int productID)
         {
             ProductModel productModel = null;
