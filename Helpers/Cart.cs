d8761808a71a79f6493a5532e25fbce199d2855f
Villar <johnvillar41@yahoo.com>
2021-04-16 12:18:31 +0800

    Added Cart class and Equals, Hash overrides on ProductModel

---
+++ Helpers/Cart.cs
@@ -0,0 +1,32 @@
+﻿using SoftEngWebEmployee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftEngWebEmployee.Helpers
+{
+    public class Cart
+    {
+        private static List<ProductModel> CartItems { get; set; }
+        public static void ClearCartItems()
+        {
+            CartItems.Clear();
+        }
+        public static void AddCartItem(ProductModel cartProduct)
+        {
+            CartItems.Add(cartProduct);
+        }
+        public static void RemoveCartItem(ProductModel cartProduct)
+        {
+            foreach(var product in CartItems)
+            {
+                if(product.Equals(cartProduct))
+                {
+                    CartItems.Remove(product);
+                    break;
+                }
+            }
+        }
+    }
+}
