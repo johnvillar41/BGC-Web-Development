05e5f2ca9cb25d22d385a29d12f16ef2c673edfe
johnvillar41 <johndanielvillar123@gmail.com>
2021-06-04 10:16:25 +0800

    Fixed validations on User profile

--- Views/UserProfile.aspx.cs
+++ Views/UserProfile.aspx.cs
@@ -39,36 +39,64 @@ namespace SoftEngWebEmployee.Views
 
         protected async void ButtonSaveProfile_Click(object sender, EventArgs e)
         {
-            UpdateProgress1.Visible = true;            
-            var updatedUser = new AdministratorModel()
+            UpdateProgress1.Visible = true;
+            var isFieldValidated = ValidateFields();
+            if (isFieldValidated)
             {
-                Username = Username.Text,
-                Fullname = Fullname.Text,
-                Password = Password.Text,
-                Email = Email.Text
-            };
-            try
-            {
-                await UserProfileRepository.SingleInstance.UpdateProfileAsync(updatedUser, UserSession.SingleInstance.GetLoggedInUser());
-            }
-            catch(MySql.Data.MySqlClient.MySqlException ex)
-            {
-                if (ex.Message.Equals($"Duplicate entry '{updatedUser.Username}' for key 'user_username'"))
+                var updatedUser = new AdministratorModel()
                 {
-                    BuildSweetAlert(updatedUser.Username);
-                    return;
+                    Username = Username.Text,
+                    Fullname = Fullname.Text,
+                    Password = Password.Text,
+                    Email = Email.Text
+                };
+                try
+                {
+                    await UserProfileRepository.SingleInstance.UpdateProfileAsync(updatedUser, UserSession.SingleInstance.GetLoggedInUser());
                 }
-            }
-            FullnameLabel.Text = updatedUser.Fullname;            
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    if (ex.Message.Equals($"Duplicate entry '{updatedUser.Username}' for key 'user_username'"))
+                    {
+                        BuildSweetAlert( "Duplicate Name!", $"Duplicate name for:{updatedUser.Username}");
+                        return;
+                    }
+                }
+                FullnameLabel.Text = updatedUser.Fullname;
+            }                     
             UpdateProgress1.Visible = false;          
         }
-        private void BuildSweetAlert(string username)
+        private bool ValidateFields()
+        {
+            if (string.IsNullOrWhiteSpace(Username.Text))
+            {
+                BuildSweetAlert("Missing field on Username!", "Please enter missing field!");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(Fullname.Text))
+            {
+                BuildSweetAlert("Missing field on Fullname!", "Please enter missing field!");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(Password.Text))
+            {
+                BuildSweetAlert("Missing field on Password!", "Please enter missing field!");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(Email.Text))
+            {
+                BuildSweetAlert("Missing field on Email!", "Please enter missing field!");
+                return false;
+            }
+            return true;
+        }
+        private void BuildSweetAlert(string title,string message)
         {
             var alert = new SweetAlertBuilder
             {
                 HexaBackgroundColor = "#fff",
-                Title = "Duplicate Name!",
-                Message = $"Duplicate name for:{username}",
+                Title = title,
+                Message = message,
                 AlertIcons = Constants.AlertStatus.warning
             };
             alert.BuildSweetAlert(this);
